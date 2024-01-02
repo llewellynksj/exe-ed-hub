72046378ad385bcf360e530adfa79d2b586ea758
Kate L <llewellyn.ksj@gmail.com>
2024-01-02 13:04:35 +0000

    Add basic styles to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
+import styles from "../styles/NavBar.module.css";
 import logo from "../assets/exeedhublogo.webp";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack className={styles.NavBar}>
       <Image src={logo} width="60" height="20" objectFit="contain" />
       <Text>NavBar</Text>
     </HStack>
