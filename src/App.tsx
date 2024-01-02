b0086a0b282d91e67f1e7619689512d2ab5ed35c
Kate L <llewellyn.ksj@gmail.com>
2024-01-02 12:11:47 +0000

    Install chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
       <h1>ExeEd Hub</h1>
+      <Button colorScheme="blue">Button</Button>
     </div>
   );
 }
