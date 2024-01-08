780e0618cd75505cfb26b3e0d49a8e01968ac4fe
Kate L <llewellyn.ksj@gmail.com>
2024-01-08 13:30:40 +0000

    Move code of conduct from register form to add review form

--- src/pages/auth/RegisterForm.tsx
+++ src/pages/auth/RegisterForm.tsx
@@ -5,15 +5,11 @@ import { Form } from "react-bootstrap";
 import { useState } from "react";
 
 const RegisterForm = () => {
-  const [openModal, setOpenModal] = useState(false);
-
-  const toggleModal = () => setOpenModal(!openModal);
-
   return (
     <Form className="overflow-hidden px-4">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" name="username" placeholder="Username" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="password1">
@@ -22,27 +18,11 @@ const RegisterForm = () => {
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="password2">
-        <Form.Label className="d-none">Password</Form.Label>
+        <Form.Label className="d-none">Confirm Password</Form.Label>
         <Form.Control
           type="password"
           name="password2"
           placeholder="Repeat Password"
-        />
-      </Form.Group>
-
-      <div className="d-inline-flex justify-content-left my-1">
-        <Button onClick={toggleModal} textColor="bg-font" bgColor="primary-bg">
-          Read Code of Conduct
-        </Button>
-      </div>
-      <Modal isOpen={openModal} onClose={toggleModal}>
-        <CodeOfConduct />
-      </Modal>
-
-      <Form.Group className="my-3" controlId="formBasicCheckbox">
-        <Form.Check
-          type="checkbox"
-          label="I agree to the ExeEd Hub Code of Conduct"
         />
       </Form.Group>
 
--- src/pages/schools/AddReviewForm.tsx
+++ src/pages/schools/AddReviewForm.tsx
@@ -1,5 +1,33 @@
+import Modal from "../../components/Modal";
+import { useState } from "react";
+import Button from "../../components/Button";
+import CodeOfConduct from "../../components/CodeOfConduct";
+import { Form } from "react-bootstrap";
+
 const AddReviewForm = () => {
-  return <div>AddReviewForm</div>;
+  const [openModal, setOpenModal] = useState(false);
+
+  const toggleModal = () => setOpenModal(!openModal);
+
+  return (
+    <div>
+      <div className="d-inline-flex justify-content-left my-1">
+        <Button onClick={toggleModal} textColor="bg-font" bgColor="primary-bg">
+          Read Code of Conduct
+        </Button>
+      </div>
+      <Modal isOpen={openModal} onClose={toggleModal}>
+        <CodeOfConduct />
+      </Modal>
+
+      <Form.Group className="my-3" controlId="formBasicCheckbox">
+        <Form.Check
+          type="checkbox"
+          label="I agree to the ExeEd Hub Code of Conduct"
+        />
+      </Form.Group>
+    </div>
+  );
 };
 
 export default AddReviewForm;
