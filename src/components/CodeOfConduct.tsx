085506ab8d7d7ec8dda5c2066d5014995cbe053a
Kate L <llewellyn.ksj@gmail.com>
2024-01-08 09:19:17 +0000

    Add code of conduct component

---
+++ src/components/CodeOfConduct.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const CodeOfConduct = () => {
+  return (
+    <div className="overflow-hidden px-4">
+      <h1>Code of Conduct</h1>
+      <ul>
+        <li>
+          Always treat others with kindness and respect, even if you don't agree
+          with them
+        </li>
+        <li>Stay on topic, don't intentionally derail threads</li>
+        <li>
+          Do not identify people using personal information including name,
+          email address, contact number etc
+        </li>
+        <li>Don't troll</li>
+        <li>Don't spam</li>
+        <li>Use appropriate language</li>
+        <li>Do not intentionally insult or offend others</li>
+        <li>
+          We do not tolerate hate speech, intimidation, harassment, sexual,
+          racial, or ethnic slurs, and discrimination.
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default CodeOfConduct;
