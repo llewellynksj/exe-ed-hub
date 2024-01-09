5585cd1ff459aca5e6c8e4b3d30e9e8786166fba
Kate L <llewellyn.ksj@gmail.com>
2024-01-09 15:42:19 +0000

    Minor amendments to attempt fix to loading bug

--- src/api/axiosDefaults.ts
+++ src/api/axiosDefaults.ts
@@ -1,8 +1,23 @@
 import axios from "axios";
 
+// Set the base URL and content type
 axios.defaults.baseURL = 'https://exe-ed-hub-07f60c90d2ab.herokuapp.com';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
+
+// Add an interceptor for each request
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
